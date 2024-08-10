4d468f2fcf5c336a55d5e5c51f1c63bfd1e22368
KumJungMin <rmawjdals@naver.com>
2024-08-10 15:42:12 +0900

    chore: add test components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineAsyncComponent } from "vue";
+
+const components = [];
+
+// Dynamically import all .vue files in the ./components directory
+const modules = import.meta.glob("./components/TestPage*.vue");
+
+for (const path in modules) {
+  const nameMatch = path.match(/\/TestPage(\d+)\.vue$/);
+  if (nameMatch) {
+    const name = `TestPage${nameMatch[1]}`;
+    components.push({
+      name,
+      component: defineAsyncComponent(modules[path]),
+    });
+  }
+}
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <div>
+    <component
+      v-for="{ name, component } in components"
+      :key="name"
+      :is="component"
+    />
+  </div>
 </template>
---
+++ src/components/TestPage251.svg
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <img v-for="i in 300" :key="i" :src="`/svg/icon${i}.svg`" alt="icon" />
+  </div>
+</template>
